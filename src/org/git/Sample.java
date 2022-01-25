2d106279537cc3fcf6591ae5ff38f2fe83951f6b
Vinoth-1122 <avinothbala91@gmail.com>
2022-01-25 22:39:20 +0530

    Adding Codes

--- src/org/git/Sample.java
+++ src/org/git/Sample.java
@@ -7,5 +7,10 @@ public class Sample {
 		System.out.println("Coding started");
 		
 		System.out.println("line 1");
+		
+		System.out.println("Pushed Code Sucessfully ");
+		
+		System.out.println("pushing New code");
+		
 	}
 }
